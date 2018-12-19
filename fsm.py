aa40e137f5592c5524841ead157fb8adb4a2abdc
tyt0429 <sugar.14387@gmail.com>
2018-12-19 14:40:49 +0800

    add send image

--- fsm.py
+++ fsm.py
@@ -1,6 +1,6 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+from utils import send_text_message, send_image_url
 
 
 class TocMachine(GraphMachine):
@@ -11,97 +11,97 @@ class TocMachine(GraphMachine):
         )
 
     def is_going_to_state_session(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '查詢演出場次'
         return False
 
     def is_going_to_state_ticket_prcie(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '查詢票價'
         return False
 
     def is_going_to_state_sponsor(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '我想贊助你們'
         return False
 
     def is_going_to_state_Jan(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '一月'
         return False
 
     def is_going_to_state_Feb(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '二月'
         return False
 
     def is_going_to_state_March(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '三月'
         return False
 
     def is_going_to_state_price1(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
-            return text.lower() == '貴賓席'
+            return text.lower() == '黃區'
         return False
 
     def is_going_to_state_price2(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
-            return text.lower() == '搖滾區'
+            return text.lower() == '紅區'
         return False
 
     def is_going_to_state_price3(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
-            return text.lower() == '一般區'
+            return text.lower() == '藍區'
         return False
 
     def is_going_to_state_buy(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '確認購買'
         return False
 
     def is_going_to_state_transfer(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '是'
         return False
 
     def is_going_to_state_sponsor1000(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '$1000'
         return False
 
     def is_going_to_state_sponsor5000(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '$5000'
         return False
 
     def is_going_to_state_sponsor10000(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '$10000'
         return False
 
     def is_going_to_state_crew_list(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '查詢工作人員名單'
         return False
 
     def is_going_to_state_detail_info(self, event):
-        if event.get("message"):
+        if event.get("message") and event['message'].get("text"):
             text = event['message']['text']
             return text.lower() == '場次詳細資訊'
         return False
@@ -119,8 +119,10 @@ class TocMachine(GraphMachine):
         print("I'm entering state_ticket_prcie")
 
         sender_id = event['sender']['id']
-        send_text_message(sender_id, "您想查詢什麼位置的票價?(貴賓席/搖滾區/一般區)")
-
+        send_image_url(sender_id, "https://ppt.cc/f3Rusx@.jpg")
+        send_text_message(sender_id, "您想查詢什麼位置的票價?(黃區/紅區/藍區)")
+        
+        		
     def on_exit_state_ticket_prcie(self, event):
         print('Leaving state_ticket_prcie')
         
@@ -128,7 +130,8 @@ class TocMachine(GraphMachine):
         print("I'm entering state_sponsor")
 
         sender_id = event['sender']['id']
-        send_text_message(sender_id, "使用轉帳(是)")
+        send_image_url(sender_id, "https://anfansy.files.wordpress.com/2015/10/1211790456.gif")
+        send_text_message(sender_id, "轉帳帳號如圖，請問是否使用轉帳(是)")
 
     def on_exit_state_sponsor(self, event):
         print('Leaving state_sponsor')
@@ -173,7 +176,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state_price1")
 
         sender_id = event['sender']['id']
-        send_text_message(sender_id, "貴賓席的票價為$800\n是否確認購買(確認購買)")
+        send_text_message(sender_id, "黃區的票價為$800\n是否確認購買(確認購買)")
 
     def on_exit_state_price1(self, event):
         print('Leaving state_price1')
@@ -182,7 +185,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state_price2")
 
         sender_id = event['sender']['id']
-        send_text_message(sender_id, "搖滾區的票價為$500\n是否確認購買(確認購買)")
+        send_text_message(sender_id, "紅區的票價為$500\n是否確認購買(確認購買)")
 
     def on_exit_state_price2(self, event):
         print('Leaving state_price2')
@@ -191,7 +194,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state_price3")
 
         sender_id = event['sender']['id']
-        send_text_message(sender_id, "一般區的票價為$300\n是否確認購買(確認購買)")
+        send_text_message(sender_id, "藍區的票價為$300\n是否確認購買(確認購買)")
 
     def on_exit_state_price3(self, event):
         print('Leaving state_price3')
@@ -240,6 +243,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state_crew_list")
 
         sender_id = event['sender']['id']
+        send_image_url(sender_id ,"https://ppt.cc/f0a3Xx")
         send_text_message(sender_id, "工作人員名單如下\n------藝術組------\n導演：湯雅瑭\n編劇：吳念真\n演員：陳怡亘 吳念真 郭予凡\n陳宜青 張瑋庭 徐子歡\n\n------技術組------\n舞台監督：黃臆璇\n舞台設計：馬佳琳\n舞台組員：蔡欣芸 張碩真 藍詩雅 林欣融\n燈光設計：李沛思\n音效設計：王凱莉\n服化妝設計：黃子菁\n服化妝組員：吳念真 吳沛潔\n\n------行政組------\n行政組長：曾恩瑜\n宣傳：馬佳琳 何竹昀 張碩真\n公關：黃沛瑀 藍詩雅\n前台：秦仕真 吳欣凌 蔡欣芸 林欣融 黃珮慈")
         self.go_back()
 
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 
 
 GRAPH_URL = "https://graph.facebook.com/v2.6"
-ACCESS_TOKEN = "EAAFClPHBVGYBAAvZAqJDCZCwWZBrH6PJqITdy7BfgL8gq9crgEeGrRmtpfBPVzd7ORHQ1nfJvZBZBjZCQrAb3osU0IPfNfEhyZBBiLX7HlsNqspBCPbPt5otq75ls76hUY9u1s2lB7vDq3l0Qzp7wOYokU7Ub3etd10enEozkr3Uf90mIaljlg2"
+ACCESS_TOKEN = "EAAFClPHBVGYBANTUliug7YgE1EFBZC235GZCfBVP11X37JNTALu4lSUbv4vPlZAnvM4hkZAm6l4ZARHpnctxRZBOspI1Q2W9sPJUs3vtNPn2ZCLBL8KYQ4GhnnP2UXS57ETINZAIVq5DnGoZBM9Fkzsrq1oKCyzMPtq6Niku9m6rZAm8GRoAvzqDtp"
 
 
 def send_text_message(id, text):
@@ -18,10 +18,57 @@ def send_text_message(id, text):
     return response
 
 
-"""
+
 def send_image_url(id, img_url):
-    pass
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient": {"id": id},
+        "message": {
+			"attachment":{
+				"type":"image",
+				"payload":{
+					"url":img_url,
+					"is_reusable":True
+			}
+		}
+    }
+	}
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+    
 
 def send_button_message(id, text, buttons):
-    pass
-"""
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient": {"id": id},
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"button",
+                    "text":"What do you want to do next?",
+                    "buttons":[
+                        {
+                            "type":"web_url",
+                            "url":"https://www.messenger.com",
+                            "title":"Visit Messenger"
+                        },
+                        {
+                            "type":"web_url",
+                            "url":"https://www.messenger.com",
+                            "title":"Visit Messenger"
+                        },
+                    ]
+                }
+            }
+        }
+    
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
